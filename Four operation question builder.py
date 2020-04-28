2df155c902eebfe586360002b03c1e69427c1c88
Cstar140 <62321086+Cstar140@users.noreply.github.com>
2020-04-28 10:22:23 +0800

    Update Four operation question builder.py

--- Four operation question builder.py
+++ Four operation question builder.py
@@ -1,5 +1,4 @@
 """
-四则运算生成器：
 能够自动生成四则运算练习题
 可以定制题目数量
 用户可以选择运算符
@@ -24,17 +23,17 @@ def create(operate, max, decimal, dec_bit, bracket):
             numBracket = 1
             braLeft1 = random.randint(1, numCount - 1)  # 随机生成括号1的位置
             braRight1 = random.randint(2, numCount)
-            while braRight1 < braLeft1:
+            while braRight1 <= braLeft1:
                 braRight1 = random.randint(2, numCount)
         else:
             numBracket = random.randint(0, 2)
             braLeft1 = random.randint(1, numCount - 1)  # 随机生成括号1的位置
             braRight1 = random.randint(2, numCount)
-            while braRight1 < braLeft1:  # 如果)在(左边，重新生成
+            while braRight1 <= braLeft1:  # 如果)在(左边，重新生成
                 braRight1 = random.randint(2, numCount)
             braLeft2 = random.randint(1, numCount - 1)  # 括号2位置
             braRight2 = random.randint(2, numCount)
-            while braRight2 < braLeft2:
+            while braRight2 <= braLeft2:
                 braRight2 = random.randint(2, numCount)
     for i in range(numCount):
         if bracket:  # 随机添加左括号
@@ -71,13 +70,12 @@ def create(operate, max, decimal, dec_bit, bracket):
 # 查看问题的解答
 def view_answers():
     value = lbox.get(lbox.curselection())  # 获取当前选中的文本
-    var1.set(round(eval(str(value)), 2))  # 为label设置值
+    var1.set(round(eval(str(value)), 4))  # 为label设置值
 
 
 # 获取输入的数据
 def inputdata():
     lbox.delete(1, END)  # 清空上次生成的题目
-
     num = var_num.get()
     num = int(num)
     operate = []
@@ -104,52 +102,55 @@ def inputdata():
 
 if __name__ == '__main__':
     # 创建窗口
+    bgcolor='#E6E6FA'# 设置背景颜色
+    blackcolor='#F5F5F5' # 设置输入框颜色
     win = tkinter.Tk()
     win.title("四则运算生成器")
     win.geometry("600x600")
+    win.configure(background=bgcolor)
     # 创建选项
-    tkinter.Label(win, text='题目数量：').place(x=10, y=150)
+    tkinter.Label(win, text='题目数量：', bg=bgcolor).place(x=10, y=150)
     var_num = tkinter.StringVar()
-    entry_num = tkinter.Entry(win, textvariable=var_num)
+    entry_num = tkinter.Entry(win, textvariable=var_num, bg=blackcolor)
     entry_num.place(x=80, y=150)
 
-    tkinter.Label(win, text='操作符：').place(x=10, y=180)
+    tkinter.Label(win, text='操作符：', bg=bgcolor).place(x=10, y=180)
     op1 = StringVar()
     op2 = StringVar()
     op3 = StringVar()
     op4 = StringVar()
-    check1 = tkinter.Checkbutton(win, text='+', font=("Times New Roman", "16"), variable=op1, onvalue='+', offvalue='').place(x=80, y=170)
-    check2 = tkinter.Checkbutton(win, text='-', font=("Times New Roman", "16"), variable=op2, onvalue='-', offvalue='').place(x=140, y=170)
-    check3 = tkinter.Checkbutton(win, text='*', font=("Times New Roman", "16"), variable=op3, onvalue='*', offvalue='').place(x=200, y=170)
-    check4 = tkinter.Checkbutton(win, text='/', font=("Times New Roman", "16"), variable=op4, onvalue='/', offvalue='').place(x=260, y=170)
+    check1 = tkinter.Checkbutton(win, text='+', font=("Times New Roman", "16"), variable=op1, onvalue='+', offvalue='', bg=bgcolor).place(x=80, y=175)
+    check2 = tkinter.Checkbutton(win, text='-', font=("Times New Roman", "16"), variable=op2, onvalue='-', offvalue='', bg=bgcolor).place(x=140, y=175)
+    check3 = tkinter.Checkbutton(win, text='*', font=("Times New Roman", "16"), variable=op3, onvalue='*', offvalue='', bg=bgcolor).place(x=200, y=175)
+    check4 = tkinter.Checkbutton(win, text='/', font=("Times New Roman", "16"), variable=op4, onvalue='/', offvalue='', bg=bgcolor).place(x=260, y=175)
 
-    tkinter.Label(win, text='数字最大值：').place(x=10, y=210)
+    tkinter.Label(win, text='数字最大值：', bg=bgcolor).place(x=10, y=210)
     var_max = tkinter.StringVar()
-    entry_max = tkinter.Entry(win, textvariable=var_max)
+    entry_max = tkinter.Entry(win, textvariable=var_max, bg=blackcolor)
     entry_max.place(x=80, y=210)
 
-    tkinter.Label(win, text='是否有小数?').place(x=10, y=240)
+    tkinter.Label(win, text='是否有小数?', bg=bgcolor).place(x=10, y=240)
     var_dec = tkinter.StringVar()
-    var_dec.set('1')
-    radio1 = tkinter.Radiobutton(win, variable=var_dec, value='1', text='是').place(x=80, y=240)
-    radio1 = tkinter.Radiobutton(win, variable=var_dec, value='0', text='否').place(x=150, y=240)
+    var_dec.set('0')
+    radio1 = tkinter.Radiobutton(win, variable=var_dec, value='1', text='是', bg=bgcolor).place(x=80, y=240)
+    radio1 = tkinter.Radiobutton(win, variable=var_dec, value='0', text='否', bg=bgcolor).place(x=150, y=240)
 
-    tkinter.Label(win, text='请输入精度：').place(x=10, y=270)
+    tkinter.Label(win, text='请输入精度：', bg=bgcolor).place(x=10, y=270)
     var_bit = tkinter.StringVar()
-    entry_bit = tkinter.Entry(win, textvariable=var_bit)
+    entry_bit = tkinter.Entry(win, textvariable=var_bit, bg=blackcolor)
     entry_bit.place(x=80, y=270)
 
-    tkinter.Label(win, text='是否有括号?').place(x=10, y=300)
+    tkinter.Label(win, text='是否有括号?', bg=bgcolor).place(x=10, y=300)
     var_bra = tkinter.StringVar()
-    var_dec.set('1')
-    radio2 = tkinter.Radiobutton(win, variable=var_bra, value='1', text='是').place(x=80, y=300)
-    radio2 = tkinter.Radiobutton(win, variable=var_bra, value='0', text='否').place(x=150, y=300)
+    var_bra.set('0')
+    radio2 = tkinter.Radiobutton(win, variable=var_bra, value='1', text='是', bg=bgcolor).place(x=80, y=300)
+    radio2 = tkinter.Radiobutton(win, variable=var_bra, value='0', text='否', bg=bgcolor).place(x=150, y=300)
 
     button_enter = tkinter.Button(win, text='点击生成题目', bg='orange', command=inputdata)
     button_enter.place(x=100, y=330)
     # 创建答案区
     var1 = tkinter.StringVar()
-    lab1 = tkinter.Label(win, bg='yellow', width=20, height=2, textvariable=var1)
+    lab1 = tkinter.Label(win, bg=blackcolor, width=20, height=2, textvariable=var1)
     lab1.pack()
     # 创建查看答案按钮
     button1 = tkinter.Button(win, text='点此查看算式答案', width=15, height=2, command=view_answers, bg='orange')
@@ -160,7 +161,7 @@ if __name__ == '__main__':
     scrolly = Scrollbar(win)
     scrolly.pack(side=RIGHT, fill=Y)
     # 创建Listbox（即生成算式列表）
-    lbox = tkinter.Listbox(win, width=30, height=200, listvariable=var2, yscrollcommand=scrolly.set)
+    lbox = tkinter.Listbox(win, width=30, height=200, listvariable=var2, yscrollcommand=scrolly.set, bg=blackcolor)
     lbox.pack(anchor=tkinter.NE)
     scrolly.config(command=lbox.yview)
     # 显示主窗口
